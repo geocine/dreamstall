4d29c6bf2d4d119ec73f6939c6660019f746cb19
Aivan Monceller <aivan@monceller.com>
2023-01-13 17:41:38 +0800

    feat(deps): move from script to requirements

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -13,5 +13,4 @@
 @CALL pip install -U diffusers
 @CALL pip install -U -r %BASE_DIR%\requirements.txt
 @CALL pip install -U -I --no-deps https://github.com/geocine/dreamstall-binaries/releases/download/c116-p310-t112/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
-@CALL pip install requests
 @CALL python %~dp0setup.py
